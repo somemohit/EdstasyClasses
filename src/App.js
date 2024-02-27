4186943c9e3107c348c2be90b410629e85116e79
somemohit <98733767+somemohit@users.noreply.github.com>
2024-02-27 21:39:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import {
 } from 'react-icons/fi';
 import {IoLogoWhatsapp} from 'react-icons/io';
 
-import {Navigation, Pagination, A11y} from 'swiper/modules';
+import {Autoplay, Navigation, Pagination, A11y} from 'swiper/modules';
 
 import {Swiper, SwiperSlide} from 'swiper/react';
 
@@ -21,15 +21,39 @@ import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 
 function App() {
+  const slideImgs = [
+    // {
+    //   id: '1',
+    //   url: './EdstasyClasses/tution.jpg',
+    //   alt: 'edstasy-1',
+    // },
+    {
+      id: '1',
+      url: 'tution.jpg',
+      alt: 'edstasy-1',
+    },
+    {
+      id: '2',
+      url: 'tution.jpg',
+      alt: 'edstasy-2',
+    },
+    {
+      id: '3',
+      url: 'tution.jpg',
+      alt: 'edstasy-3',
+    },
+  ];
+
   return (
     <div>
       <nav className="bg-gray-200 p-4">
-        <div className="mx-auto flex justify-between items-center">
+        <div className="mx-auto flex flex-col md:flex-row justify-between md:items-center gap-2">
           <div className="flex items-center gap-4">
             <div className="text-white h-12 w-12 sm:h-16 sm:w-16 md:h-20 md:w-20">
               <img
                 className="w-full h-full object-contain"
-                src="./EdstasyClasses/logo.png"
+                // src="./EdstasyClasses/logo.png"
+                src="logo.png"
                 alt="edstasy-logo"
               />
             </div>
@@ -37,17 +61,17 @@ function App() {
               Edstasy Classes
             </div>
           </div>
-          <div className="flex items-center gap-4">
+          <div className="flex items-center gap-4 pl-2">
             <div className="flex items-center">
               <FiMail className="text-gray-700" />
-              <span className="text-gray-700 ml-1 hidden sm:inline">
-                info@edstasy.com
+              <span className="text-gray-700 text-xs sm:text-sm md:text-base ml-1 inline">
+                satyabrata@gmail.com
               </span>
             </div>
             <div className="flex items-center">
               <FiPhone className="text-gray-700" />
-              <span className="text-gray-700 ml-1 hidden sm:inline">
-                +1 234 567 890
+              <span className="text-gray-700 text-xs sm:text-sm md:text-base ml-1 inline">
+                +91 9785214568
               </span>
             </div>
           </div>
@@ -55,31 +79,36 @@ function App() {
       </nav>
 
       <main className="mx-auto">
-        <Swiper
-          modules={[Navigation, Pagination, A11y]}
-          spaceBetween={50}
-          slidesPerView={1}
-          navigation
-          pagination={{clickable: true}}
-          onSwiper={(swiper) => console.log(swiper)}
-          onSlideChange={() => console.log('slide change')}
-        >
-          <SwiperSlide>
-            <img
-              className="w-full h-96 object-contain"
-              src="tution.jpg"
-              alt="edstasy-logo"
-            />
-          </SwiperSlide>
-          <SwiperSlide>Slide 2</SwiperSlide>
-          <SwiperSlide>Slide 3</SwiperSlide>
-          <SwiperSlide>Slide 4</SwiperSlide>
-        </Swiper>
-        <div className="h-auto md:h-96 bg-blue-500 p-10 text-center">
-          <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold mb-4">
-            Welcome to my website
+        <div className="">
+          <Swiper
+            modules={[Autoplay, Navigation, Pagination, A11y]}
+            spaceBetween={50}
+            slidesPerView={1}
+            navigation
+            pagination={{clickable: true}}
+            autoplay={{delay: 3000}}
+            onSwiper={(swiper) => console.log(swiper)}
+            onSlideChange={() => console.log('slide change')}
+          >
+            {slideImgs?.map((item) => {
+              return (
+                <SwiperSlide key={item?.id}>
+                  <img
+                    className="w-full h-80 md:h-[550px] object-contain"
+                    src={item?.url}
+                    alt={item?.alt}
+                  />
+                </SwiperSlide>
+              );
+            })}
+          </Swiper>
+        </div>
+
+        <div className="h-auto md:h-96 bg-blue-500 p-10 text-center flex flex-col md:gap-6 justify-center items-center">
+          <h1 className="text-white text-4xl md:text-5xl lg:text-6xl font-bold mb-4">
+            About us
           </h1>
-          <p className="text-lg md:text-xl lg:text-2xl px-4 md:px-0">
+          <p className="text-white text-sm md:text-xl lg:text-2xl px-4 md:px-0">
             At Edstasy Classes, we believe in fostering a dynamic learning
             environment where every student is empowered to achieve their
             academic goals and unlock their full potential. With a commitment to
@@ -89,7 +118,7 @@ function App() {
           </p>
         </div>
 
-        <div className="parallax-bg h-auto md:h-96 flex flex-col md:flex-row justify-center items-center md:gap-20 relative overflow-hidden">
+        <div className="parallax-bg py-10 h-auto md:h-96 flex flex-col md:flex-row justify-center items-center md:gap-20 relative overflow-hidden">
           <div className="flex flex-col justify-center items-center mb-8 md:mb-0">
             <div className="w-28 h-28 md:w-44 md:h-44 flex items-center justify-center bg-blue-500 p-2 md:p-4 rounded-full">
               <img
@@ -128,48 +157,76 @@ function App() {
             </p>
           </div>
         </div>
+
+        <div className="h-96 bg-blue-500 p-10 flex justify-center items-center">
+          <iframe
+            title="my-loc"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14926.661394566492!2d86.41384019898022!3d20.723809693601797!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a1be8874e719bdd%3A0x3cfa88f6a15f4a30!2sGoudasahi%2C%20Odisha%20755004!5e0!3m2!1sen!2sin!4v1709048799478!5m2!1sen!2sin"
+            allowfullscreen=""
+            loading="lazy"
+            className="w-11/12 md:w-1/2 h-full"
+            referrerpolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </div>
       </main>
 
-      <footer className="bg-gray-200 p-4 mt-8">
-        <div className="mx-auto flex flex-col md:flex-row justify-between items-center gap-10 text-black">
+      <footer className="bg-gray-200">
+        <div className="mx-auto h-fit md:h-44 flex flex-col md:flex-row justify-between items-center gap-10 text-black p-10">
+          <div className="text-white h-24 w-24 sm:h-28 sm:w-28 md:h-32 md:w-32">
+            <img
+              className="w-full h-full object-contain"
+              // src="./EdstasyClasses/logo.png"
+              src="logo.png"
+              alt="edstasy-logo"
+            />
+          </div>
+
           <div className="flex items-center gap-4">
-            <FiMapPin className="text-gray-700" />
+            <FiMapPin className="text-gray-700 text-xl" />
             <span className="text-sm sm:text-base md:text-lg">
               123 Main Street, Cityville, State, Country
             </span>
           </div>
-          <div className="flex items-center gap-4">
-            <a
-              href="https://www.instagram.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FiInstagram className="text-gray-700" />
-            </a>
-            <a
-              href="https://www.youtube.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FiYoutube className="text-gray-700" />
-            </a>
-            <a
-              href="https://twitter.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FiTwitter className="text-gray-700" />
-            </a>
+
+          <div className="flex flex-col items-center gap-4">
+            <p className="text-2xl font-bold">Follow us on</p>
+            <div className="flex items-center gap-4">
+              <a
+                href="https://www.instagram.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FiInstagram className="text-gray-700 text-2xl" />
+              </a>
+              <a
+                href="https://www.youtube.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FiYoutube className="text-gray-700 text-2xl" />
+              </a>
+              <a
+                href="https://twitter.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FiTwitter className="text-gray-700 text-2xl" />
+              </a>
+            </div>
           </div>
+        </div>
+
+        <div className="bg-gray-400 h-16 flex justify-center items-center">
+          Edstasy Classes -2024 © All Rights Reserved.
         </div>
       </footer>
 
       <a
-        href="https://api.whatsapp.com/send?phone=1234567890"
+        href="https://api.whatsapp.com/send?phone=7077586648"
         target="_blank"
         rel="noopener noreferrer"
         title="whatsapp me"
-        className="fixed bottom-4 right-4 border border-green-500 p-2 rounded-full"
+        className="fixed bottom-4 right-4 border border-green-500 bg-white p-2 rounded-full"
       >
         <IoLogoWhatsapp className="text-green-500 text-3xl md:text-4xl" />
       </a>
