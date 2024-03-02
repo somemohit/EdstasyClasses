1ec3aff27d2e39a4697c21c2557af20a139a2ef8
somemohit <98733767+somemohit@users.noreply.github.com>
2024-03-02 14:04:54 +0530

    netlify commit our features modify

---
+++ src/StaticData.js
@@ -0,0 +1,45 @@
+export const slideImgs = [
+  // {
+  //   id: '1',
+  //   url: './EdstasyClasses/tution.jpg',
+  //   alt: 'edstasy-1',
+  // },
+  {
+    id: '1',
+    url: '/slide/img1.jpg',
+    alt: 'edstasy-1',
+  },
+  {
+    id: '2',
+    url: '/slide/img2.jpg',
+    alt: 'edstasy-2',
+  },
+  {
+    id: '3',
+    url: '/slide/poster.png',
+    alt: 'edstasy-3',
+  },
+];
+
+export const menus = [
+  {
+    id: '1',
+    name: 'Profile',
+  },
+  {
+    id: '2',
+    name: 'Features',
+  },
+  {
+    id: '3',
+    name: 'About',
+  },
+  {
+    id: '4',
+    name: 'Location',
+  },
+  {
+    id: '5',
+    name: 'Contact',
+  },
+];
